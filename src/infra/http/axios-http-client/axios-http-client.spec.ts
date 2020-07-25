944f0f318a4f19705239293509ba6479107e7f34
Luan Fernandes <luan.fj01@gmail.com>
2020-07-25 14:31:42 -0300

    test: ensure AxiosHttpClient calss axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import faker from 'faker';
+
+import { AxiosHttpClient } from './axios-http-client';
+
+jest.mock('axios');
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('AxiosHttpClient', () => {
+  test('Should call Axios with correct URL', async () => {
+    const url = faker.internet.url();
+
+    const sut = new AxiosHttpClient();
+
+    await sut.post({ url });
+
+    expect(mockedAxios).toHaveBeenCalledWith(url);
+  });
+});
